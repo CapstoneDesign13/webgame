9dea6ae9c8b6d5f698f80f49b078474729783e00
Inbyungjun <2002jun7@gmail.com>
2026-05-24 19:34:25 +0900

    SO제거

--- Scripts/PlayerSetting.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-[CreateAssetMenu(menuName = "Setting/Player")]
-public class PlayerSetting : UnitSettingSO
-{
-    public int duelturn = 5;
-}
--- Scripts/PlayerSettingSO.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-[System.Serializable]
-[CreateAssetMenu(menuName = "Setting/Player")]
-public class PlayerSettingSO : UnitSettingSO
-{
-    public int duelturn = 5;
-}
